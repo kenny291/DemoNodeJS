7bf3b6d9a3abcd0141205335775bbff6cb214ed6
Khiem291 <khiem291@yahoo.com>
2017-07-12 14:30:37 +0700

    include navigation

--- index.js
+++ index.js
@@ -1,36 +1,48 @@
-var express = require('express');
-var mongoose = require('mongoose');
+let express = require('express');
+let mongoose = require('mongoose');
+let bodyParser = require('body-parser');
 
-var app = express();
-mongoose.connect('mongodb://localhost/my_db', {useMongoClient: true});
+let app = express();
+mongoose.connect('mongodb://localhost/mydb', {useMongoClient: true});
 
-var personSchema = mongoose.Schema({
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+let personSchema = mongoose.Schema({
    name: String,
    age: Number,
    nationality: String
 });
-var Person = mongoose.model("Person", personSchema);
+let Person = mongoose.model("Person", personSchema);
 app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+   res.render('person');
+});
 app.get('/person', function(req, res){
    res.render('person');
 });
 
 app.post('/person', function(req, res){
-   var personInfo = req.body; //Get the parsed information
-   res.send(personInfo)
+   let personInfo = req.body; //Get the parsed information
+   console.log(personInfo)
+
    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
       res.render('show_message', {
-         message: "Sorry, you provided worng info", type: "error"});
-   } else {
-      var newPerson = new Person({
+         message: "Sorry, you provided wrong info", type: "error"});
+   }
+   else {
+      let newPerson = new Person({
          name: personInfo.name,
          age: personInfo.age,
          nationality: personInfo.nationality
       });
 		
       newPerson.save(function(err, Person){
-         if(err)
-            res.render('show_message', {message: "Database error", type: "error"});
+         if(err){
+            console.log(err)
+            res.render('show_message', {message: "Database error: %s" % err, type: "error"});
+         }
          else
             res.render('show_message', {
                message: "New person added", type: "success", person: personInfo});
